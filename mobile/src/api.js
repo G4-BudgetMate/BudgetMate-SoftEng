12147272c19ff7796606df631d4a50bf9435a28d
MizukiYuta <boylesjhunlloyd@gmail.com>
2025-11-26 23:51:35 +0800

    change ip address for api.js

--- mobile/src/api.js
+++ mobile/src/api.js
@@ -5,9 +5,9 @@ import { Platform } from "react-native";
 // Choose the correct baseURL depending on where the app is running
 const baseURL = Platform.select({
   web: "http://localhost:5000",     // Expo Web (browser)
-  ios: "http://175.176.84.3:5000",     // iOS simulator
-  android: "http://175.176.84.3:5000",  // Android emulator
-  default: "http://175.176.84.3:5000",
+  ios: "http://175.176.85.226:5000",     // iOS simulator
+  android: "http://175.176.85.226:5000",  // Android emulator
+  default: "http://175.176.85.226:5000",
 });
 
 console.log("API baseURL =", baseURL);
